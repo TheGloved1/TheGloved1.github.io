f99139637a9d41d2251d83a98a7fe33de6b85743
TheGloved1 <gloves1229@gmail.com>
2024-01-02 20:55:50 -0600

    Sections appear relative to screen center

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
-    });
-});
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
+    },
+    {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+    }
+);
 
 const hiddenElements = document.querySelectorAll(".hidden");
 
